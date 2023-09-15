04fa9406501cb27b74792f68555694dffcb26edb
Durian Sosa <dmsosa21@outlook.com>
2023-09-15 16:55:04 +0100

    Initial setup

---
+++ src/main/java/com/duvi/myessen/MyessenApplication.java
@@ -0,0 +1,13 @@
+package com.duvi.myessen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyessenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyessenApplication.class, args);
+	}
+
+}
