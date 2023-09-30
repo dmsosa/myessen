b82a1a3534749972e924f01b6446f0e4278a6a1f
Durian Sosa <dmsosa21@outlook.com>
2023-09-30 14:55:39 +0100

    Use case verschwinden

--- server/src/main/java/com/duvi/myessen/core/useCases/SearchFoodUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.duvi.myessen.core.useCases;
-
-import com.duvi.myessen.core.entities.Food;
-
-public interface SearchFoodUseCase {
-    public Food searchFoodByName(String name);
-    public Food searchFoodById(Long id);
-    }
-
