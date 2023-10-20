d93cc5252b2cb8cd020212333f3efb95550810ac
Durian Sosa <dmsosa21@outlook.com>
2023-10-20 06:47:41 +0100

    Navbar reparieren

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -14,21 +14,19 @@ function Navbar() {
     const handleTheme = () => {
         const isCurrentDark = theme === "dark";
         setTheme( isCurrentDark ? "light" : "dark" );
-        localStorage.setItem("default-theme3e", isCurrentDark ? "light" : "dark" );
+        localStorage.setItem("default-theme", isCurrentDark ? "light" : "dark" );
     }
 
-    /* <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent"> */
     return (
+        
         <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
                 <Link className="navbar-brand" to="/">
                     <img src={apple} className="navbar-brand-logo"/>MyEssen
                 </Link>
                 <DropdownMenu />
-
+            </div>
         </nav>
     );
 }
