a007947f07cf3c429b0f85a6401546f46a869059
Durian Sosa <dmsosa21@outlook.com>
2023-11-07 07:47:02 +0000

    Navbar Toggler setted

--- client/src/components/Navbar/DropdownMenu.tsx
+++ client/src/components/Navbar/DropdownMenu.tsx
@@ -19,14 +19,21 @@ function DropdownMenu() {
     }
 
     const handleClick = () => {
+        document.body.classList.toggle('body-noscroll', dropdown);
         setDropdown((prev: boolean) => !prev)
+    }
+
+    const toggleLogo = (logo, setDropdown) => {
+        return ( 
+            <span className={!dropdown ? "bi-globe" : "navbar-toggler-icon"}></span>
+        )
     }
 
     return (
         <>
         <div className="toggler-container d-lg-none d-inline-flex justify-content-end position-relative mv-0">
-            <button className="navbar-toggler nav-main-collapse d-lg-none d-inline-flex" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+            <button className="navbar-toggler nav-main-collapse d-lg-none d-inline-flex" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={handleClick}>
+                    {toggleLogo("start", setDropdown)}
             </button>
         </div>
         <div className="collapse navbar-collapse navbar-collapse-main navbar-indicator collapse show" id="navbarSupportedContent">
